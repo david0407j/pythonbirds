bac1ccb57d0add849a9d53421d8202a08741c801
david <djunio239@gmail.com>
2022-08-06 20:01:45 -0300

    criados atribudo de instância e nome idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,21 @@
 class Pessoa:
-    def __init__(self,nome=None, idade=50):
+    def __init__(self, nome = None, idade=50):
         self.idade = idade
         self.nome = nome
 
-    def Cumprimentar(self):
-        return f'olá {id.self}'
+
+    def cumprimentar(self):
+        return f'olá {id(self)}'
 
 if __name__ == '__main__':
     p = Pessoa('luciano')
-    p = Pessoa()
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
-    print(pnome)
-    p.nome= 'davidson'
+    print(p.nome)
+    p.nome = 'davidson'
     print(p.nome)
     print(p.idade)
+
+
+
